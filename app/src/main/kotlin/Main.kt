bb3f1ea37a121f855d77886b6163aaa8970036bf
ya0igoddess <matratskingalya@gmail.com>
2023-06-30 12:36:38 +0300

    member join events

--- app/src/main/kotlin/Main.kt
+++ app/src/main/kotlin/Main.kt
@@ -1,11 +1,12 @@
 import com.github.ya0igoddess.dbsync.DBSyncExtension
 import com.kotlindiscord.kord.extensions.ExtensibleBot
-import org.koin.core.context.startKoin
 
 suspend fun main(args: Array<String>) {
     val token = System.getenv("SKAARD_TOKEN")
     val bot = ExtensibleBot(token) {
-        extensions { add(::DBSyncExtension) }
+        extensions {
+            add(::DBSyncExtension)
+        }
     }
     bot.start()
 }
--- database-sync/src/main/kotlin/com/github/ya0igoddess/dbsync/Extension.kt
+++ database-sync/src/main/kotlin/com/github/ya0igoddess/dbsync/Extension.kt
@@ -1,13 +1,39 @@
 package com.github.ya0igoddess.dbsync
 
 import com.github.ya0igoddess.dbsync.config.DBSyncModule
-import com.github.ya0igoddess.dbsync.database.dataBaseModule
+import com.github.ya0igoddess.dbsync.repositories.IDiscordGuildRepoService
+import com.github.ya0igoddess.dbsync.repositories.IDiscordUserRepoService
+import com.kotlindiscord.kord.extensions.events.ExtensionStateEvent
 import com.kotlindiscord.kord.extensions.extensions.Extension
+import com.kotlindiscord.kord.extensions.extensions.ExtensionState
+import com.kotlindiscord.kord.extensions.extensions.event
+import com.kotlindiscord.kord.extensions.utils.respond
+import dev.kord.core.event.guild.GuildCreateEvent
+import dev.kord.core.event.guild.MemberJoinEvent
+import dev.kord.core.event.message.MessageCreateEvent
+import kotlinx.coroutines.flow.collect
+import org.koin.core.component.inject
+import java.util.logging.Logger
 
 class DBSyncExtension: Extension() {
     override val name: String = "db-sync"
 
     override suspend fun setup() {
         getKoin().loadModules(listOf(DBSyncModule))
+
+        val userService: IDiscordUserRepoService by inject()
+        val guildService: IDiscordGuildRepoService by inject()
+
+        event<MemberJoinEvent> {
+            action {
+                userService.createFromExternalEntity(event.member.asUser())
+            }
+        }
+
+        event<GuildCreateEvent> {
+            action {
+                guildService.getOrCreateFromExternal(event.guild)
+            }
+        }
     }
 }
