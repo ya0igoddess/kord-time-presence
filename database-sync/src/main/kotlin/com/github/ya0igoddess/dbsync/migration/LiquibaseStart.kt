427ef48b1c106c98bdba35c9a4bc71e5ac695028
ya0igoddess <matratskingalya@gmail.com>
2023-09-21 23:28:25 +0300

    fixed schema quoting

--- database-sync/src/main/kotlin/com/github/ya0igoddess/dbsync/migration/LiquibaseStart.kt
+++ database-sync/src/main/kotlin/com/github/ya0igoddess/dbsync/migration/LiquibaseStart.kt
@@ -1,25 +1,24 @@
 package com.github.ya0igoddess.dbsync.migration
 
 import com.github.ya0igoddess.dbsync.config.settings.DBConnectionSettings
-import liquibase.Contexts
 import liquibase.Scope
 import liquibase.database.DatabaseFactory
+import liquibase.database.ObjectQuotingStrategy
 import liquibase.database.jvm.JdbcConnection
 import liquibase.resource.ClassLoaderResourceAccessor
-import java.io.Writer
 import java.sql.Connection
 import java.sql.DriverManager
 
 fun loadLiquibase(settings: DBConnectionSettings, schemaCode: String, changeLogFile: String) {
     Scope.child(mapOf()) {
 
-        //val adaptedSchemaCode = schemaCode.replace("-", "_")
-        val connection: Connection? = DriverManager.getConnection(settings.url, settings.user, settings.password)
+        val fixedSchemaCode = schemaCode.replace("-","_") //liquibase cant handle quotes correctly
+        val connection: Connection = DriverManager.getConnection(settings.url, settings.user, settings.password)
+        connection.prepareStatement("create schema if not exists $fixedSchemaCode").execute()
         val database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(JdbcConnection(connection))
-        connection?.prepareStatement("create schema if not exists $schemaCode")!!.execute()
-        database.defaultSchemaName = schemaCode
+        database.objectQuotingStrategy = ObjectQuotingStrategy.QUOTE_ALL_OBJECTS
+        database.defaultSchemaName = fixedSchemaCode
         val liquibase = liquibase.Liquibase(changeLogFile, ClassLoaderResourceAccessor(), database)
-        //liquibase.update(Contexts(), Writer.nullWriter())
         liquibase.update()
     }
 }
