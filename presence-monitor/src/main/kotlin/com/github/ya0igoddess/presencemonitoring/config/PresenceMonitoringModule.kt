bba204caaed6b6b95ad9d3a9e34c83da2aa12318
ya0igoddess <matratskingalya@gmail.com>
2023-09-23 00:53:59 +0300

    ChannelPresence init interModule interaction

---
+++ presence-monitor/src/main/kotlin/com/github/ya0igoddess/presencemonitoring/config/PresenceMonitoringModule.kt
@@ -0,0 +1,8 @@
+package com.github.ya0igoddess.presencemonitoring.config
+
+import com.github.ya0igoddess.dbsync.config.DBSyncModule
+import org.koin.dsl.module
+
+val PresenceMonitoringModule = module {
+    includes(DBSyncModule)
+}
