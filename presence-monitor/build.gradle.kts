9efbf00e52bd7997c9f2a74b0c6f86439af172c0
ya0igoddess <70133277+ya0igoddess@users.noreply.github.com>
2024-03-26 23:55:10 +0300

    add kotest lib

--- presence-monitor/build.gradle.kts
+++ presence-monitor/build.gradle.kts
@@ -5,6 +5,7 @@ val mockkVersion: String by properties
 plugins {
     kotlin("jvm")
     kotlin("plugin.serialization")
+    id("io.kotest")
 }
 
 repositories {
@@ -15,7 +16,8 @@ dependencies {
     implementation(project(":database-sync"))
     implementation("org.ufoss.kotysa:kotysa-r2dbc:$koTySaVersion")
 
-    testImplementation("io.kotest:kotest-runner-junit5-jvm:$kotestVersion")
     testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+    testImplementation("io.kotest:kotest-framework-engine-jvm:$kotestVersion")
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:$kotestVersion")
     testImplementation("io.mockk:mockk:$mockkVersion")
 }
