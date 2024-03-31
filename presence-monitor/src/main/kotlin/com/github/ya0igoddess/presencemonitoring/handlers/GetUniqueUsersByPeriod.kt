8ab02797ea84a7a2ffeeccb79dc6f39e3b651f21
ya0igoddess <matratskingalya@gmail.com>
2024-03-31 15:43:24 +0300

    use DtOrDttmConverter in handlers

--- presence-monitor/src/main/kotlin/com/github/ya0igoddess/presencemonitoring/handlers/GetUniqueUsersByPeriod.kt
+++ presence-monitor/src/main/kotlin/com/github/ya0igoddess/presencemonitoring/handlers/GetUniqueUsersByPeriod.kt
@@ -1,23 +1,20 @@
 package com.github.ya0igoddess.presencemonitoring.handlers
 
+import com.github.ya0igoddess.dbsync.converters.dateOrDateTime
 import com.github.ya0igoddess.dbsync.model.discord.lvalue
 import com.github.ya0igoddess.dbsync.repositories.IDiscordChannelRepoService
 import com.github.ya0igoddess.presencemonitoring.service.IVoiceConnectionUserService
 import com.kotlindiscord.kord.extensions.commands.Arguments
 import com.kotlindiscord.kord.extensions.commands.application.slash.PublicSlashCommand
 import com.kotlindiscord.kord.extensions.commands.converters.impl.channel
-import com.kotlindiscord.kord.extensions.commands.converters.impl.string
 import com.kotlindiscord.kord.extensions.components.forms.ModalForm
 import com.kotlindiscord.kord.extensions.extensions.Extension
 import com.kotlindiscord.kord.extensions.extensions.publicSlashCommand
-import kotlinx.datetime.Instant
-import kotlinx.datetime.TimeZone
-import kotlinx.datetime.toLocalDateTime
 
 class GetUniqueUsersByPeriodArgs : Arguments() {
     val channel by channel { name = "channel"; description = "Channel to get info" }
-    val start by string { name = "period_end"; description = "Period start" }
-    val end by string { name = "period_start"; description = "Period end" }
+    val start by dateOrDateTime { name = "start"; description = "Period start" }
+    val end by dateOrDateTime { name = "end"; description = "Period end" }
 }
 
 
@@ -29,9 +26,8 @@ internal suspend fun Extension.RegisterGetUniqueUsersByPeriodCommand(
         name = "get_unique_users"
         description = "Get list of active users in given period"
         action {
-            val timeZone = TimeZone.currentSystemDefault()
-            val start = Instant.parse(arguments.start).toLocalDateTime(timeZone)
-            val end = Instant.parse(arguments.end).toLocalDateTime(timeZone)
+            val start = arguments.start
+            val end = arguments.end
             val channel = requireNotNull(channelRepo.getById(arguments.channel.id.lvalue))
             respond {
                 content = service.getUniqueMembersByPeriod(channel, start, end).map { it.name }.toString()
